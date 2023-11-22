3ed13dab9a48a1e7ca2b6e85185e7b6b89835802
fgorsky <fedor@support-pc.org>
2023-11-22 11:46:26 +0300

    ./gradlew clean build success

--- src/test/java/SkillBox/com/users/UsersApplicationTests.java
+++ src/test/java/SkillBox/com/users/UsersApplicationTests.java
@@ -2,8 +2,10 @@ package SkillBox.com.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = {PostgreSQLInitializer.class})
 class UsersApplicationTests {
 
 	@Test
